846601f66cb604e158ad5def733b12e2440081c4
r.mirzoyan <rusmir@ukr.net>
2019-04-10 10:00:00 +0300

    forumClasses

---
+++ src/lesson6/forum/ForumPollsOption.java
@@ -0,0 +1,10 @@
+package lesson6.forum;
+
+import java.util.Date;
+
+public class ForumPollsOption {
+    private long id;
+    private ForumPost post;
+    private String title;
+    private Date date;
+}
---
+++ src/lesson6/forum/ForumSubcategory.java
@@ -0,0 +1,12 @@
+package lesson6.forum;
+
+import java.util.Date;
+
+public class ForumSubcategory {
+    private long id;
+    private ForumCategory category;
+    private String title;
+    private String description;
+    private Date date;
+    private String ip;
+}
